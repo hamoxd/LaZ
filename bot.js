3d09c93597bc2eca9bfe7370cdb97774f6a3c4f4
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-19 09:22:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', msg => {
   }
 });
 
-var prefix = "**";
+var prefix = "#";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -44,7 +44,6 @@ client.on("message", message => {
  message.delete(); 
 };     
 });
-
 
 
 
