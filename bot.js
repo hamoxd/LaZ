f6867a8e4aae75ce258f105ed0abca6798e0764e
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-19 09:33:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -666,18 +666,7 @@ message.channel.sendEmbed(cat);
     }
 });
 
-client.on('message', message => {
-var prefix = "^^";
 
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("**bc <message>**");
-return;
-}
         message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
             var bc = new Discord.RichEmbed()
